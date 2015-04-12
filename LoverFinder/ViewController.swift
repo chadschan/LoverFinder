45ec32f470eda58cee4842216c866f70876e9243
chad <chad@chads-iMac.local>
2015-04-12 16:34:42 +0800

    Initial Commit

---
+++ LoverFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoverFinder
+//
+//  Created by chad on 15/4/12.
+//  Copyright (c) 2015年 chad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
